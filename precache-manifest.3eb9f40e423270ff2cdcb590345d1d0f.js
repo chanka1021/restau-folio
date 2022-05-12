3f07a3ce03e08e1fb404fbdbdf7767b43462cb92
chanka1021 <achraf.chouach@gmail.com>
2022-05-12 15:29:42 +0100

    Updates

---
+++ precache-manifest.3eb9f40e423270ff2cdcb590345d1d0f.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "0bdf8571cf582f0ac618",
+    "url": "/restau-folio/static/css/main.747fcf25.chunk.css"
+  },
+  {
+    "revision": "0bdf8571cf582f0ac618",
+    "url": "/restau-folio/static/js/main.0bdf8571.chunk.js"
+  },
+  {
+    "revision": "6e1ff50c878455c5bc73",
+    "url": "/restau-folio/static/js/1.6e1ff50c.chunk.js"
+  },
+  {
+    "revision": "cb701a5d235654f3c6f4",
+    "url": "/restau-folio/static/js/runtime~main.cb701a5d.js"
+  },
+  {
+    "revision": "86b19d8ecb90e35ca2a22e79af5d23e0",
+    "url": "/restau-folio/static/media/button_line.86b19d8e.png"
+  },
+  {
+    "revision": "27a604979b8f249314c6c7b9e0dbd1e9",
+    "url": "/restau-folio/static/media/about01.27a60497.jpg"
+  },
+  {
+    "revision": "f866bf755bd3748f457bfca9072ac661",
+    "url": "/restau-folio/static/media/about02.f866bf75.jpg"
+  },
+  {
+    "revision": "2f5ffc1756f3e0d155cfbb864c76a9c4",
+    "url": "/restau-folio/static/media/about03.2f5ffc17.jpg"
+  },
+  {
+    "revision": "c3b3e3aa93d683138307d6f2a2d33501",
+    "url": "/restau-folio/static/media/about04.c3b3e3aa.jpg"
+  },
+  {
+    "revision": "3001032f6384715714a0564184730c8b",
+    "url": "/restau-folio/static/media/about05.3001032f.jpg"
+  },
+  {
+    "revision": "2b662366c91cfd9ade83da62467cf04a",
+    "url": "/restau-folio/static/media/about06.2b662366.jpg"
+  },
+  {
+    "revision": "e2431a60f0a5c35735e158499ceb5000",
+    "url": "/restau-folio/static/media/parallax02.e2431a60.jpg"
+  },
+  {
+    "revision": "0781304e07d39d73ddb0285e57ae275f",
+    "url": "/restau-folio/static/media/gallery01.0781304e.jpg"
+  },
+  {
+    "revision": "40e51aff55fa7d99d15fcc092aec789d",
+    "url": "/restau-folio/static/media/gallery02.40e51aff.jpg"
+  },
+  {
+    "revision": "c018ef44b580f50ebded2c8724ddab50",
+    "url": "/restau-folio/static/media/gallery03.c018ef44.jpg"
+  },
+  {
+    "revision": "f7e51f2d1c611881d04ff5bafb73dee5",
+    "url": "/restau-folio/static/media/gallery04.f7e51f2d.jpg"
+  },
+  {
+    "revision": "16e6a6416e1fff97567cad11b878a2ec",
+    "url": "/restau-folio/static/media/gallery05.16e6a641.jpg"
+  },
+  {
+    "revision": "f00b01eb8376a1d5f189b48ba7677970",
+    "url": "/restau-folio/static/media/gallery06.f00b01eb.jpg"
+  },
+  {
+    "revision": "0d8a2c5fe8bc55e3fe63e57ba4b472d2",
+    "url": "/restau-folio/static/media/gallery07.0d8a2c5f.jpg"
+  },
+  {
+    "revision": "7a572692a46a633dd20afdd9545584c4",
+    "url": "/restau-folio/static/media/gallery08.7a572692.jpg"
+  },
+  {
+    "revision": "ddff2c8fdd1987a00618cf1aad6f6416",
+    "url": "/restau-folio/static/media/gallery09.ddff2c8f.jpg"
+  },
+  {
+    "revision": "41a3fa20f7c6d16ee50930595ca83479",
+    "url": "/restau-folio/static/media/logo_intro.41a3fa20.png"
+  },
+  {
+    "revision": "089c0f640e9f7e00d01975ee14b43336",
+    "url": "/restau-folio/static/media/home1.089c0f64.jpg"
+  },
+  {
+    "revision": "30742bea1206a1e123ad1d247341b3a1",
+    "url": "/restau-folio/static/media/hom2.30742bea.jpg"
+  },
+  {
+    "revision": "8aabe5ed69d6a2ec81369d8719abf5ee",
+    "url": "/restau-folio/static/media/hom3.8aabe5ed.jpg"
+  },
+  {
+    "revision": "1413c06e50c0480bf8ba31a1ae3b2edb",
+    "url": "/restau-folio/static/media/parallax01.1413c06e.jpg"
+  },
+  {
+    "revision": "962b53f6e4bbc8008658308951828a5b",
+    "url": "/restau-folio/static/media/mainMenu01.962b53f6.png"
+  },
+  {
+    "revision": "1403a27454b617777c5d3e181515ccda",
+    "url": "/restau-folio/static/media/mainMenu02.1403a274.png"
+  },
+  {
+    "revision": "ea0ddcc2b99c1269e29e1d6b711f531b",
+    "url": "/restau-folio/static/media/mainMenu03.ea0ddcc2.png"
+  },
+  {
+    "revision": "26d55050e82fbf66e8dbaf1dd7556d72",
+    "url": "/restau-folio/index.html"
+  }
+];
